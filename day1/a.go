7555bb223441f214a0317bd48e57023881ddb2a7
Andrea Björnsdóttir <andrea.bjoernsdottir@hexagon.com>
2021-12-03 09:31:15 +0100

    Advent Of Code: Day1

---
+++ day1/a.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "bufio"
+  "strconv"
+)	
+
+func main() {
+    if len(os.Args) < 2 {
+        fmt.Println("Missing parameter, provide file name!")
+        return
+    }
+
+    f, err := os.Open(os.Args[1])
+    if err != nil {
+        fmt.Println("Can't open file:", os.Args[1])
+        panic(err)
+    }
+
+    r := bufio.NewReader(f)
+    line, err := Readln(r)
+
+    // Count initialized as -1 to account
+    // for the first value being counted as
+    // an increase
+    var (
+        count int = -1
+        prevVal int = -1
+        val int = 0
+    )
+    for err == nil {
+        val, err = strconv.Atoi(line)
+        if val > prevVal {
+            count++
+        }
+
+        prevVal = val
+        // Read next, if hasNext:
+        line, err = Readln(r)
+    }
+
+    fmt.Printf("Number of increases: %d", count)
+}
+
+func Readln(r *bufio.Reader) (string, error) {
+  var (isPrefix bool = true
+       err error = nil
+       line, ln []byte
+      )
+
+  for isPrefix && err == nil {
+      line, isPrefix, err = r.ReadLine()
+      ln = append(ln, line...)
+  }
+  return string(ln),err
+}
---
+++ day1/b.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "bufio"
+  "strconv"
+)	
+
+func main() {
+    if len(os.Args) < 2 {
+        fmt.Println("Missing parameter, provide file name!")
+        return
+    }
+
+    f, err := os.Open(os.Args[1])
+    if err != nil {
+        fmt.Println("Can't open file:", os.Args[1])
+        panic(err)
+    }
+
+    r := bufio.NewReader(f)
+    line, err := Readln(r)
+
+    var (
+      val int = 0
+      numbers []int = make([]int, 0)
+    )
+    for err == nil {
+        val, err = strconv.Atoi(line)
+        numbers = append(numbers, val)
+        line, err = Readln(r)
+    }
+    
+    count, err := CountIncreasedWindows(&numbers)
+    if err != nil {
+      fmt.Println("Error counting number of increases")
+      panic(err)
+    }
+    fmt.Printf("Number of increases: %d", count)
+}
+
+func CountIncreasedWindows(numbers *[]int) (int, error) {
+    var (
+      start int = 0
+      end int = 2
+      lastSum int = 0
+      sum int = 0
+      count int = 0
+    )
+
+    // Calculate initial sum
+    for i := 0; i <= end; i++ {
+      sum += (*numbers)[i]
+    }
+
+    for end < len(*numbers)-1 {
+      lastSum = sum
+      end++
+      sum += (*numbers)[end]
+      sum -= (*numbers)[start]
+      start++
+
+      if sum > lastSum {
+        count++
+      }
+
+    }
+
+    return count, nil
+}
+
+func Readln(r *bufio.Reader) (string, error) {
+  var (isPrefix bool = true
+       err error = nil
+       line, ln []byte
+      )
+
+  for isPrefix && err == nil {
+      line, isPrefix, err = r.ReadLine()
+      ln = append(ln, line...)
+  }
+  return string(ln),err
+}
