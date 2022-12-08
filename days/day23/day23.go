b991120a80bbf8f9ac24dff1ae7d3bd8dbeea82b
Andrea Bjoernsdottir <andreabjork43@gmail.com>
2022-12-08 17:07:10 +0100

    wip day 23

--- days/day23/day23.go
+++ days/day23/day23.go
@@ -10,9 +10,25 @@ func Day23(inputFile string, part int) {
 	fmt.Println("not implemented")
 }
 
+func (s *State) print() {
+	fmt.Printf("#############\n")
+	fmt.Printf("#")
+	for i := 0; i < len(s.hallway); i++ {
+		fmt.Printf("%s", string(s.hallway[i]))
+	}
+	fmt.Printf("\n ")
+	for i := 0; i < len(s.topRooms); i++ {
+		fmt.Printf("#%s", string(s.topRooms[i]))
+	}
+
+	fmt.Printf("\n ")
+	for i := 0; i < len(s.bottomRooms); i++ {
+		fmt.Printf("#%s", string(s.topRooms[i]))
+	}
+	fmt.Printf("#########")
+}
+
 func parseState(inputFile string) *State{
-	// ###B#C#B#D###
-  //   #A#D#C#A#
 	ls := util.LineScanner(inputFile)
 	//#############
 	//#...........#
@@ -21,27 +37,27 @@ func parseState(inputFile string) *State{
 	// 	#########
 	_, _ = util.Read(ls)
 	_, ok := util.Read(ls)
-	
+
+	hallway := make([]rune, 11)
 	top, _ := util.Read(ls)
 	bottom, _ := util.Read(ls)
 	re := regexp.MustCompile(`#+([A-D])#([A-D])#([A-D])$([A-D])#+`)
+	topRooms := re.FindStringSubmatch(top)	
+	bottomRooms := re.FindStringSubmatch(bottom)	
+	amphipods := make([]*Amphipod, 8)
+	energies := map[rune]int{'A': 1, 'B': 10, 'C': 100, 'D': 1000}
 	
-	for i := 0; i < 2; i++ {
-		line, _ := util.Read(ls)
-		aps := re.FindStringSubmatch(line)
-
-		for j := 0; j < len(aps); j++ {
-
-		}
-
-	}
+	for top := amphipod
+	amphipods[i*j] = &Amphipod{rType, energies[rType], i+1,j}	
 	re.FindStringSubmatch(bottom)
 	
-	
-
+	// Return the initial game state
 	return &State{
 		score,
 		amphipods,
+		hallway,
+		topRooms,
+		bottomRooms,
 		0,
 		nil,
 		[]*State{},
@@ -67,6 +83,9 @@ func optimize() {
 // Decision Tree with nodes containing 'State'
 type State struct {
 	score 							int // 
+	hallway 						[]rune
+	topRooms						[]rune
+	bottomRooms					[]rune
 	amphipods						[]*Amphipod
 	fuel								int
 	fromState 					*State
@@ -76,12 +95,12 @@ type State struct {
 
 // Depth First Search of the optimum, constructing the
 // tree as we go
-func (s *State) DFS(fromNode *State) {
-	if s.score == 0 {
-		if s.fuel <= min {
-			min = s.fuel
-		}
-	}
+//func (s *State) DFS(fromNode *State) {
+//	if s.score == 0 {
+//		if s.fuel <= min {
+//			min = s.fuel
+//		}
+//	}
 
 	// if score == 0, stop and track minimum
 	// if score >= 0 but we've exceeded known minimum, stop
@@ -92,7 +111,7 @@ func (s *State) DFS(fromNode *State) {
 	// 
 	// Any unblocked amphipod moves to any* possible position,
 	// so long as that position makes sense.
-}
+//}
 
 // Add the next states in the tree, taking care
 // to limit states reasonably
@@ -108,18 +127,24 @@ func (s *State) show() {
 // Amphipod
 // ========
 type Amphipod struct {
-	vpos int // 0 for being in room, 1 for hallway
-	hpos int // number of room, or number of spot in hallway counting from left to right
+	subtype rune
+	energy  int
+	vpos 		int // 0 for being in room, 1 for hallway
+	hpos 		int // number of room, or number of spot in hallway counting from left to right
 }
 
-func (a *Amphipod) walk() {
-	
+func (a *Amphipod) getEnergy() rune {
+	return []rune{'A': 1, 'B': 10, 'C': 100, 'D': 1000}[a.subtype]
 }
 
-func (a *Amphipod) isBlocked() {
-
-}
-
-func (a *Amphipod) canGoHome() {
-
-}
+//func (a *Amphipod) walk() {
+//	
+//}
+//
+//func (a *Amphipod) isBlocked() {
+//
+//}
+//
+//func (a *Amphipod) canGoHome() {
+//
+//}
